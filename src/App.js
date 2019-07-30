4cbb26c08b7e279b866db9028cc08d599fd17322
Ubuntu <ubuntu@ip-172-31-35-218.us-east-2.compute.internal>
2019-07-30 00:36:42 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { motion, AnimatePresence } from "framer-motion"
 
 function App() {
   return (
-    <div className="App">
+    <AnimatePresence>
+    <div className="App-header">
+    <motion.div initial={{scale: 0.9, y: 50, opacity: 0}} animate={{scale: 1, y:0, opacity: 1}}transition = {{ delay: .5 }}
+ className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <motion.div
+        drag
+        dragConstraints={{top: 0, right: 0, bottom: 0, left: 0}}
+        whileTap={{scale: 0.75}} whileHover={{scale: 1.2}} transition={{type: "spring", stiffness: 200, damping: 20}}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </motion.div>
+        <motion.p animate={{y: 0}} initial={{y: 50}}
+>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </motion.p>
+        <motion.a whileTap={{scale: 0.9}} whileHover={{scale: 1.05}} animate={{y: 0}} initial={{y: 50}}
           className="App-link"
-          href="https://reactjs.org"
+          href="https://murden.dev"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener"
         >
-          Learn React
-        </a>
+          React Developer
+        </motion.a>
       </header>
+    </motion.div>
     </div>
+  </AnimatePresence>
   );
 }
 
