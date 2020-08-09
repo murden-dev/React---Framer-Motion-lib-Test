8db7ad10fecdd4258d09a596d6b7b9b9c3c3c438
Matheus Murden <matheus@murden.dev>
2020-08-09 10:52:29 -0300

    Updated dependencies and animation timings.

--- src/AppScreen.js
+++ src/AppScreen.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Button, TextField } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import { purple } from '@material-ui/core/colors';
 
 import './App.scss';
 import { motion } from "framer-motion"
@@ -22,14 +21,14 @@ class AppScreen extends Component {
       <motion.p
       initial={{ y: -50, opacity: 0}}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ delay: 1.6, duration: .4, type: "spring", stiffness: 100, mass: 1, damping: 15 }}
+      transition={{ delay: 0.8, duration: .4, type: "spring", stiffness: 100, mass: 1, damping: 15 }}
       >
         File management is so 2018...
       </motion.p>
       <motion.h1
       initial={{ y: -50, opacity: 0}}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ delay: 2.6, duration: .2, type: "spring", stiffness: 100, mass: 3, damping: 25 }}
+      transition={{ delay: 1.6, duration: .2, type: "spring", stiffness: 100, mass: 3, damping: 25 }}
       >
         We know<span>.</span>
       </motion.h1>
@@ -37,7 +36,7 @@ class AppScreen extends Component {
       <motion.div // linha horizontal acima
         initial={{ y: -10, opacity: 0}}
         animate={{ y: 0, opacity: 1}}
-        transition={{ delay: 4.2, duration: .5, staggerChildren: 0.2}}
+        transition={{ delay: 2.6, duration: .5, staggerChildren: 0.2}}
         >
           <svg width="375" height="147" viewBox="0 0 375 147" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M171.326 146.713C156.243 146.725 141.264 144.211 127.011 139.276C107.904 132.671 92.2507 123.561 80.4866 112.198C58.2337 90.7048 39.9897 87.4656 28.6056 88.5322C5.45162 87.8059 0.303145 110.448 0.232166 110.511L-0.258057 109.965C-0.185646 109.9 5.11071 87.1149 28.4895 87.8059C35.8478 87.1083 43.5427 88.3238 51.3611 91.4198C61.111 95.2806 71.0819 102.094 80.9967 111.67C92.6833 122.958 108.246 132.013 127.251 138.582C162.141 150.642 201.464 147.722 235.136 130.571C246.285 124.892 257.304 121.015 267.887 119.05C281.231 116.57 293.887 117.145 305.505 120.759C331.035 128.7 348.473 128.279 358.606 126.528C369.54 124.639 374.5 120.858 374.549 120.82L375 121.399C374.795 121.559 369.844 125.331 358.731 127.251C348.518 129.016 330.955 129.444 305.287 121.46C279.297 113.377 252.975 122.308 235.47 131.225C218.827 139.747 200.653 144.873 182.009 146.303C178.435 146.576 174.874 146.713 171.326 146.713Z" fill="#3F3D56"/>
@@ -49,7 +48,7 @@ class AppScreen extends Component {
           <motion.div class="box"
           initial={{y: 100, x: 0, opacity: 0}}
           animate={{y: 0, x: [0, 5, -5, 0], opacity: 1}}
-          transition={{ delay: 4.2, duration: .8, type: "spring", mass: 2, damping: 50 }}
+          transition={{ delay: 2.6, duration: .6, type: "spring", mass: 2, damping: 50 }}
           >
             <svg width="375" height="147" viewBox="0 0 375 147" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M115.903 1.03226H63.2581V52.6452H115.903V1.03226Z" fill="#F2F2F2"/>
@@ -63,7 +62,7 @@ class AppScreen extends Component {
           <motion.div class="box"
           initial={{y: 100, x: 0, opacity: 0}}
           animate={{y: 0, x: [0, 5, -5, 0], opacity: 1}}
-          transition={{ delay: 4.4, duration: .8, type: "spring", mass: 2, damping: 50 }}
+          transition={{ delay: 2.8, duration: .6, type: "spring", mass: 2, damping: 50 }}
           >
             <svg width="375" height="147" viewBox="0 0 375 147" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M218.097 26.8387H165.452V78.4516H218.097V26.8387Z" fill="#F2F2F2"/>
@@ -77,7 +76,7 @@ class AppScreen extends Component {
           <motion.div class="box"
           initial={{y: 100, x: 0, opacity: 0}}
           animate={{y: 0, x: [0, 5, -5, 0], opacity: 1}}
-          transition={{ delay: 4.6, duration: .8, type: "spring", mass: 2, damping: 50 }}
+          transition={{ delay: 3, duration: .6, type: "spring", mass: 2, damping: 50 }}
           >
             <svg width="375" height="147" viewBox="0 0 375 147" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M320.29 0H267.645V52.6452H320.29V0Z" fill="#F2F2F2"/>
@@ -93,7 +92,7 @@ class AppScreen extends Component {
         <motion.div class="top"
         initial={{x: 0, y: '-10%', opacity: 0}}
         animate={{x: 0, y: '100%', opacity: 1}}
-        transition={{ delay: 3.5, duration: .4, type: "spring", mass: 1, damping: 20 }}
+        transition={{ delay: 2.6, duration: .4, type: "spring", mass: 1, damping: 20 }}
         >
           <svg width="100" height="36" viewBox="0 0 100 36" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M99.9468 10.9328H0.471191V35.7308H99.9468V10.9328Z" fill="#3F3D56"/>
@@ -105,7 +104,7 @@ class AppScreen extends Component {
         <motion.div class="left"
         initial={{x: -100, y: '153%', opacity: 0}}
         animate={{x: 0, y: '153%', opacity: 1}}
-        transition={{ delay: 3.5, duration: .4, type: "spring", mass: 1, damping: 20 }}
+        transition={{ delay: 2.6, duration: .4, type: "spring", mass: 1, damping: 20 }}
         >
           <svg width="147" height="67" viewBox="0 0 147 67" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M146.489 0.315994H0.815918V66.5592H146.489V0.315994Z" fill="#3F3D56"/>
@@ -120,7 +119,7 @@ class AppScreen extends Component {
         <motion.div class="right"
         initial={{x: 100, y: '90%', opacity: 0}}
         animate={{x: 0, y: '95%', opacity: 1}}
-        transition={{ delay: 3.5, duration: .4, type: "spring", mass: 1, damping: 20 }}
+        transition={{ delay: 2.6, duration: .4, type: "spring", mass: 1, damping: 20 }}
         >
           <svg width="89" height="111" viewBox="0 0 89 111" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M88.4135 110.541H0.768921L25.3818 67.9213L44.5912 34.6566L88.4135 110.541Z" fill="#3F3D56"/>
@@ -131,7 +130,7 @@ class AppScreen extends Component {
         <motion.div class="absolute center" // triângulo do meio
         initial={{x: '-50%', y: 100, opacity: 0}}
         animate={{x: '-50%', y: -15, opacity: 1}}
-        transition={{ delay: 3.5, duration: .2, type: "spring", stiffness: 100, mass: 1, damping: 25 }}
+        transition={{ delay: 2.6, duration: .2, type: "spring", stiffness: 100, mass: 1, damping: 25 }}
         >
           <svg width="122" height="120" viewBox="0 0 122 120" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M121.522 119.596H0.582642L0.678087 119.431L40.3413 50.747L61.0521 14.8803L97.9769 78.8261L121.489 119.541L121.522 119.596Z" fill="#CFFF63"/>
@@ -143,7 +142,7 @@ class AppScreen extends Component {
         class="chao"
         initial={{y: '342%', x: '-50%', opacity: 0}}
         animate={{y: '340%', x: '-50%', opacity: 1}}
-        transition={{ delay: 3.5, duration: .3, type: "spring", mass: 2, damping: 50 }}
+        transition={{ delay: 2.6, duration: .3, type: "spring", mass: 2, damping: 50 }}
         >
           <svg width="276" height="27" viewBox="0 0 276 27" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M276 26H0V27H276V26Z" fill="#3F3D56"/>
@@ -156,7 +155,7 @@ class AppScreen extends Component {
       class="form"
       initial={{y: 50, opacity: 0}}
       animate={{y: 0, opacity: 1}}
-      transition={{ delay: 4.6, duration: .2, type: "spring", mass: 1, damping: 25 }}
+      transition={{ delay: 3, duration: .2, type: "spring", mass: 1, damping: 25 }}
       >
           <TextField
           label="E-mail"
